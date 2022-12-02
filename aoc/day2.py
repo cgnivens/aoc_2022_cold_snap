38f07afd1fe7df71484540c50650d069790479ed
cgnivens <cory@workbravely.com>
2022-12-02 15:04:45 -0500

    Finished part 2 of day 2

--- aoc/day2.py
+++ aoc/day2.py
@@ -25,11 +25,37 @@ The third round is a draw with both players choosing Scissors, giving you a scor
 In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
 
 What would your total score be if everything goes exactly according to your strategy guide?
-"""
 
+--- Part Two ---
+The Elf finishes helping with the tent and sneaks back over to you. "Anyway, the second column says how the round needs to end: X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win. Good luck!"
+
+The total score is still calculated in the same way, but now you need to figure out what shape to choose so the round ends as indicated. The example above now goes like this:
+
+In the first round, your opponent will choose Rock (A), and you need the round to end in a draw (Y), so you also choose Rock. This gives you a score of 1 + 3 = 4.
+In the second round, your opponent will choose Paper (B), and you choose Rock so you lose (X) with a score of 1 + 0 = 1.
+In the third round, you will defeat your opponent's Scissors with Rock for a score of 1 + 6 = 7.
+Now that you're correctly decrypting the ultra top secret strategy guide, you would get a total score of 12.
+
+Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+"""
+from enum import Enum 
+from collections import namedtuple
+
+Options = namedtuple('Options', ['win', 'tie', 'loss'])
+
+
+class Choice(Enum):
+    A = Options(2, 1, 3)
+    B = Options(3, 2, 1)
+    C = Options(1, 3, 2)
+
+
+# part 1
 CHOICES = {letter: score for score, letter in enumerate('XYZ', start=1)}
 WINS = {('A', 'Y'), ('B', 'Z'), ('C', 'X')}
 TIES = set(zip('ABC', 'XYZ'))
+OUTCOMES = {letter: score * 3 for score, letter in enumerate('XYZ')}
+CONDITIONS = {'X': 'loss', 'Y': 'tie', 'Z': 'win'}
 
 def process_file(fh):
     for line in fh:
@@ -52,12 +78,28 @@ def part1(strategy):
     return score
 
 
+def part2(strategy):
+    score = 0
+    for opponent, outcome in strategy:
+        points = OUTCOMES[outcome]
+        condition = CONDITIONS[outcome]
+        points += getattr(Choice[opponent].value, condition)
+        score += points
+
+    return score
+        
+
+
+
 def main(datafile):
     with open(datafile) as fh:
         strategy = list(process_file(fh))
 
     print('Part 1')
     print(part1(strategy))
+
+    print("Part 2")
+    print(part2(strategy))
 
     
 
